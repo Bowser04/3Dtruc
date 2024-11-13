f8e7ea5a3151450e404a25028ad2dc6ff02412ce
Bowser04 <82542192+Bowser04@users.noreply.github.com>
2024-11-13 20:51:54 +0100

    fgfg

--- Menu/Menu.gd
+++ Menu/Menu.gd
@@ -8,27 +8,10 @@ func _ready():
 	$Multi.hide()
 	$Jouer.show()
 	$Waiting.hide()
-	multiplayer.peer_connected.connect(peer_connected)
-	multiplayer.peer_disconnected.connect(peer_disconnected)
-	multiplayer.connected_to_server.connect(connected_to_server)
-	multiplayer.connection_failed.connect(connection_failed)
-# this get called on the server and clients
-func peer_connected(id):
-	print("Player Connected " + str(id))
-	$Waiting/Label.text = "Player: "+str(0)
-# this get called on the server and clients
-func peer_disconnected(id):
-	print("Player Disconnected " + str(id))
-# called only from clients	
-func connected_to_server():
-	print("connected To Sever!")
-# called only from clients
-func connection_failed():
-	print("Couldnt Connect")
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
 	pass
-@rpc("any_peer","call_local")
+	
 func StartGame():
 	var scene = load("res://Map.tscn").instantiate()
 	get_tree().root.add_child(scene)
@@ -45,36 +28,10 @@ func _on_jouer_pressed():
 
 
 func _on_join_pressed():
-	Network_Conection.ip=$Multi/VBoxContainer/TextEdit.text
-	Network_Conection.port=int($Multi/VBoxContainer/TextEdit3.text)
-	print(Network_Conection.ip)
-	peer = ENetMultiplayerPeer.new()
-	peer.create_client(Network_Conection.ip, Network_Conection.port)
-	peer.get_host()
-	multiplayer.set_multiplayer_peer(peer)
-	$Quitter.hide()
-	$Option.hide()
-	$Jouer.hide()
-	$Multi.hide()
-	$Waiting.show()
-	pass # Replace with function body.
+	Network_Conection.join_lobby(int($Multi/VBoxContainer/TextEdit3.text))
 
 func _on_host_pressed():
-	Network_Conection.ip=$Multi/VBoxContainer/TextEdit.text
-	Network_Conection.port=int($Multi/VBoxContainer/TextEdit3.text)
-	peer = ENetMultiplayerPeer.new()
-	var error = peer.create_server(Network_Conection.port, 10)
-	if error != OK:
-		print("cannot host: " + str(error))
-		return
-	
-	multiplayer.set_multiplayer_peer(peer)
-	print("Waiting For Players!")
-	$Quitter.hide()
-	$Option.hide()
-	$Jouer.hide()
-	$Multi.hide()
-	$Waiting.show()
+	Network_Conection.create_lobby()
 	pass
 
 func _on_label_gui_input(event):
--- Menu/network.gd
+++ Menu/network.gd
@@ -8,6 +8,7 @@ var lobby_id = 0
 var lobby_members = []
 var lobby_member_max = 10
 var steam_username= ""
+var steam_id: int = 0
 
 #“call_local” means it will only run locally on the machine the script itself is on.
 #“call_remote” is to only run it remotely on other machines.
@@ -18,6 +19,8 @@ func _ready():
 	OS.set_environment("SteamAppID",str(480))
 	OS.set_environment("SteamGameID",str(480))
 	Steam.lobby_created.connect(_on_lobby_created)
+	Steam.lobby_joined.connect(_on_lobby_joined)
+	Steam.p2p_session_request.connect(_on_p2p_session_request)
 	initialize_steam()
 	pass # Replace with function body.
 
@@ -25,6 +28,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	Steam.run_callbacks()
+	if lobby_id > 0:
+		read_all_p2p_packets()
 func initialize_steam():
 	var initialize_response: Dictionary = Steam.steamInitEx()
 	print("Did Steam initialize?: %s " % initialize_response)
@@ -42,6 +47,7 @@ func _on_lobby_created(connect: int, this_lobby_id: int):
 		lobby_id = this_lobby_id
 		Steam.setLobbyJoinable(lobby_id,true)
 		Steam.setLobbyData(lobby_id,"name",steam_username+"'s server")
+		print("CREATED LOBBY: ",lobby_id)
 		var set_relay: bool = Steam.allowP2PPacketRelay(true)
 func join_lobby(this_lobby_id:int):
 	Steam.joinLobby(this_lobby_id)
@@ -49,6 +55,10 @@ func join_lobby(this_lobby_id:int):
 func _on_lobby_joined(this_lobby_id:int, _permissions: int, _locked : bool, response: int):
 	if response == Steam.CHAT_ROOM_ENTER_RESPONSE_SUCCESS:
 		lobby_id = this_lobby_id
+		get_lobby_members()
+		print("JOIN LOBBY: ",lobby_id)
+		make_p2p_handshake()
+		
 		
 func get_lobby_members():
 	lobby_members.clear()
@@ -62,5 +72,42 @@ func get_lobby_members():
 			"steam_name":member_steam_name
 		})
 		
+func make_p2p_handshake():
+	send_p2p_packet(0,{"message":"handshake", "steam_id":steam_id,"username":steam_username})
+		
 func send_p2p_packet(this_target:int, packet_data: Dictionary, send_type: int = 0):
 	var channel: int = 0
+	var this_data: PackedByteArray
+	this_data.append_array(var_to_bytes(packet_data))
+	if this_target == 0:
+		if lobby_members.size() > 1:
+			for member in lobby_members:
+				if member['steam_id'] != steam_id:
+					Steam.sendP2PPacket(member[steam_id],this_data,send_type,channel)
+	else:
+		Steam.sendP2PPacket(this_target,this_data,send_type,channel)
+
+func _on_p2p_session_request(remote_id: int):
+	var this_requester: String = Steam.getFriendPersonaName(remote_id)
+	Steam.acceptP2PSessionWithUser(remote_id)
+
+func read_all_p2p_packets(read_count: int = 0):
+	if read_count >= PACKET_READ_LIMIT:
+		return
+	if Steam.getAvailableP2PPacketSize(0) > 0:
+		read_p2p_packet()
+		read_all_p2p_packets(read_count+1)
+
+func read_p2p_packet():
+	var packet_size: int = Steam.getAvailableP2PPacketSize(0)
+	if packet_size > 0:
+		var this_packet: Dictionary = Steam.readP2PPacket(packet_size,0)
+		var packet_sender: int = this_packet['remote_steam_id']
+		
+		var packet_code: PackedByteArray = this_packet['data']
+		var readable_data: Dictionary = bytes_to_var(packet_code)
+		if readable_data.has("message"):
+			match readable_data["message"]:
+				"handshake":
+					print("PLAYER: ",readable_data["username"], " HAS JOINED!!")
+					get_lobby_members()
